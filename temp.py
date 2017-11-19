7c42639d5918130adb4fa2845247fdb629dd0618
DESKTOP-U973K82\Barbara <barbara.marquez@outlook.com.br>
2017-11-19 15:47:11 -0200

    Tá feito esse caralho \o/

--- temp.py
+++ temp.py
@@ -1,122 +1,147 @@
 # -*- coding: utf-8 -*-
 """
-Jogo da velha
+Jogo da velha - python funcional
+Alunas:
+    Ana Paula Fidelis
+    Bárbara Marquez
 """
-def makeBoard() :
-    theBoard = {};
-    rows = ['S','M','I']
-    cols = ['E','M','D']
-    for r in rows :
-        for c in cols :
-            key = str(r) + '-' + str(c)
-            theBoard.setdefault(key,False)
+def criarQuadro():
+    quadro = {};
+    linhas = ['S','M','I']
+    colunas = ['E','M','D']
+    for linha in linhas:
+        for coluna in colunas:
+            key = str(linha) + '-' + str(coluna)
+            quadro.setdefault(key,False)
     
-    return theBoard
+    return quadro
 
-def printCell(cell) :
-    if cell : 
-        return ' '+str(cell)+' '
-    else :
+def escreverCelula(celula):
+    if celula: 
+        return ' '+str(celula)+' '
+    else:
         return '   '
 
-def showBoard(board) : 
-    print(printCell(board['S-E'])+'|'+printCell(board['S-M'])+'|'+printCell(board['S-D']))
+def mostrarQuadro(quadro):
+    print(escreverCelula(quadro['S-E'])+'|'+escreverCelula(quadro['S-M'])+'|'+escreverCelula(quadro['S-D']))
     print('-----------')
-    print(printCell(board['M-E'])+'|'+printCell(board['M-M'])+'|'+printCell(board['M-D']))
+    print(escreverCelula(quadro['M-E'])+'|'+escreverCelula(quadro['M-M'])+'|'+escreverCelula(quadro['M-D']))
     print('-----------')
-    print(printCell(board['I-E'])+'|'+printCell(board['I-M'])+'|'+printCell(board['I-D']))
+    print(escreverCelula(quadro['I-E'])+'|'+escreverCelula(quadro['I-M'])+'|'+escreverCelula(quadro['I-D']))
 
-def determineVictory(board):
-    rows = ['S','M','I']
-    cols = ['E','M','D']
+def verificarVitoria(quadro):
+    linhas = ['S','M','I']
+    colunas = ['E','M','D']
     #se uma linha tiver todas as colunas iguais é vitoria
-    for r in rows :
-        col1 = str(r)+'-E'
-        col2 = str(r)+'-M'
-        col3 = str(r)+'-D'
-        if board[col1] == board[col2] == board[col3] and bool(board[col1]) :
-            return board[col3]
+    for linha in linhas:
+        col1 = str(linha)+'-E'
+        col2 = str(linha)+'-M'
+        col3 = str(linha)+'-D'
+        if quadro[col1] == quadro[col2] == quadro[col3] and bool(quadro[col1]):
+            return quadro[col3]
     #se uma coluna tiver todas as linhas iguais é vitoria
-    for c in cols :
-        row1 = 'S-'+str(c)
-        row2 = 'M-'+str(c)
-        row3 = 'I-'+str(c)
-        if board[row1] == board[row2] == board[row3] and bool(board[row1]) :
-            return board[row3]
+    for coluna in colunas:
+        lin1 = 'S-'+str(coluna)
+        lin2 = 'M-'+str(coluna)
+        lin3 = 'I-'+str(coluna)
+        if quadro[lin1] == quadro[lin2] == quadro[lin3] and bool(quadro[lin1]):
+            return quadro[lin3]
     #se houver uma diagonal com todas iguais então é vitoria
     middle = 'M-M';
-    sel1, sel2 = str(rows[0]+'-'+cols[2]), str(rows[2]+'-'+cols[0])
-    if board[middle] == board[sel1] == board[sel2] :
-       return board[middle]
-    sel1, sel2 = str(rows[0]+'-'+cols[0]), str(rows[2]+'-'+cols[2])
-    if board[middle] == board[sel1] == board[sel2] :
-       return board[middle]
+    sel1, sel2 = str(linhas[0]+'-'+colunas[2]), str(linhas[2]+'-'+colunas[0])
+    if quadro[middle] == quadro[sel1] == quadro[sel2]:
+       return quadro[middle]
+    sel1, sel2 = str(linhas[0]+'-'+colunas[0]), str(linhas[2]+'-'+colunas[2])
+    if quadro[middle] == quadro[sel1] == quadro[sel2]:
+       return quadro[middle]
     return False
 
-def determineAvailableMoves(board) :
-    rows = ['S','M','I']
-    cols = ['E','M','D']
-    hasMoves = False
+def verificarMovimentosDisponiveis(quadro):
+    linhas = ['S','M','I']
+    colunas = ['E','M','D']
+    possuiMovimentos = False
     #verifica se posicão escolhida é válida
-    for r in rows :
-        for c in cols :
-            key = str(r) + '-' + str(c)
-            if not bool(board[key]) :
+    for linha in linhas:
+        for coluna in colunas:
+            key = str(linha) + '-' + str(coluna)
+            if not bool(quadro[key]):
                 return True
-    return hasMoves
+    return possuiMovimentos
 
-def readInputAndTryToPutOnBoard(board, player) :
-    rows = ['S','M','I']
-    cols = ['E','M','D']
-    print('Para fazer um movimento, digite uma linha (S [superior], M [meio], I [inferior]), seguido por um dash (-) e uma coluna (E [esquerda], M [meio], D [direita]), como exemplo: S-M')
-    move = input()
+def processaEntrada(quadro, jogador):
+    linhas = ['S','M','I']
+    colunas = ['E','M','D']
+    print('Para fazer um movimento, digite uma linha (S [superior], M [meio], I [inferior]), seguido por um traço (-) e uma coluna (E [esquerda], M [meio], D [direita]), como exemplo: S-M')
+    posicao = input()
     valida = False
+    
     # verifica se posicão escolhida é válida
-    for r in rows :
-        for c in cols :
+    for r in linhas:
+        for c in colunas:
             key = str(r) + '-' + str(c)
-            if key == move:
+            if key == posicao:
                 valida = True
-
-    if valida & bool(board[move]):
-        print('Essa posição já está ocupada! :( Vamos tentar mais uma vez?');
+    
+    try:
+        if valida & bool(quadro[posicao]):
+            print('Essa posição já está ocupada!:( Vamos tentar mais uma vez?');
+            return False
+        elif valida:
+            quadro[posicao] = jogador
+            return True
+    except:
+        print('Posição inválida! Vamos tentar de novo? :D')
         return False
-    elif valida:
-        board[move] = player
-        return True
-    else:
-        print('Posição inválida!');
-        return False
 
-def changePlayer(player) :
-    if player == 'X':
+def mudarJogador(jogador):
+    if jogador == 'X':
         print('Jogador O, sua vez! :D ')
         return 'O'
     else:
         print('Jogador X, sua vez! ;) ')
         return 'X'
+
+def formatarCamposMap(a, b):
+  return a + '-' + b
+
+def imprimirInstrucoes():
+    linhas = ['S','M','I']
+    colunas = ['E','M','D']
+    result = zip(linhas, colunas)
     
-def game():
-    print('Vamos jogar! Jogador X, você começa ;) ')
-    player = 'X'
-    board = makeBoard();
-    while True :
-        showBoard(board);
+    print('JOGO DA VELHA')
+    print('As regras são extremamente simples: o vencedor é quem conseguir fechar primeiro uma sequencia de 3 símbolos iguais, em uma linha, coluna ou diagonal.')
+    print('E se empatar? É velha ;) ')
+    print('Ainda não sabe como jogar? Aí vai um exemplo de combinação possível:')
+    for x, y in result:
+        print(set(map(formatarCamposMap, x, y)))
+    print('É isso aí! Vamos jogar? Jogador X, você começa ;) ')
+
+def jogarJogoDaVelha():
+    imprimirInstrucoes();
+    #inicializa com o jogador X
+    jogador = 'X'
+    #cria o quadro
+    quadro = criarQuadro();
+    while True:
+        #mostra o quadro
+        mostrarQuadro(quadro);
         #obtem movimento
-        while True : 
-            move = readInputAndTryToPutOnBoard(board, player)
-            if move :
+        while True: 
+            move = processaEntrada(quadro, jogador)
+            if move:
                 break
-        #verifica se movimento concedeu vitoria
-        if determineVictory(board) :
-            showBoard(board);
-            print('Jogador '+player+' é o vencedor!!! :D ')
+        #verifica se movimento implica em finalização do jogo
+        if verificarVitoria(quadro):
+            mostrarQuadro(quadro);
+            print('Jogador '+jogador+' é o vencedor!!! :D ')
             return True
         #verifica se ainda há movimentos possíveis
-        if not determineAvailableMoves(board) :
+        if not verificarMovimentosDisponiveis(quadro):
             print('E não é que deu velha? ;) ')
             return True
-        #caso não tenha vencido então muda jogador e tenta denovo
-        player = changePlayer(player)
+        #caso não tenha vencido então muda jogador e segue o jogo
+        jogador = mudarJogador(jogador)
         
-game();
+#bora jogar essa coisa maravilhosa
+jogarJogoDaVelha();
